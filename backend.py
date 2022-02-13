59fe3eeb8cb1821408fd8394e9b7731388a55c86
VectorTensor <comppro54@gmail.com>
2022-02-13 14:44:26 +0545

    added table creator and insert user functions

--- backend.py
+++ backend.py
@@ -7,23 +7,48 @@ import sqlite3
 def create():
 	# This creates the tables
 	con = sqlite3.connect('WonderOfU.db')
-	con.execute('''CREATE TABLE Project (Name varchar,id int,Description varchar)''')
+	con.execute('''CREATE TABLE Project (Name varchar,id INTEGER PRIMARY KEY AUTOINCREMENT,Description varchar)''')
+	con.execute('''CREATE TABLE Log(DOR date,Project_id int,Log varchar, Author_id int )''')
+	con.execute('''CREATE TABLE Programmer(Name varchar, id INTEGER PRIMARY KEY AUTOINCREMENT, password varchar)''')
 	con.commit()
 	con.close()
+
+def insertUser(Name,password):
+	# This method creates a new user 
+	con = sqlite3.connect('WonderOfU.db')
+	con.execute("""INSERT INTO Programmer(Name,password) values(:name,:password)""",{"name":Name,"password":password})
+	con.commit()
+	con.close()	
+
+	# call this method to check the credentials	
+def IsUser(Name,password):
+	con = sqlite3.connect("WonderOfU.db")
+	datas=con.execute("""SELECT * from Programmer where Name=:name""",{"name":Name})
+	x=(list(datas))
+
+	
+	if (len(x)>0):
+
+		for data in x:
+		
+			if (data[2]== password):
+
+				con.commit()
+				con.close()	
+# Set name 
+				return True
+	
+	con.commit()
+	con.close()	
+	return False
 
 
 
 def drop():
 	con= sqlite3.connect('WonderOfU.db')
 	con.execute('''drop table Project''' )
-	con.commit()
-	con.close()
-
-
-def show():
-	con=sqlite3.connect('WonderOfU.db')
-	x=con.execute('''Select * from Project''')
-	print(x)
+	con.execute('''drop table Programmer''' )
+	con.execute('''drop table Log''' )
 	con.commit()
 	con.close()
 
---
+++ test.py
@@ -0,0 +1,11 @@
+from backend import *
+
+create()
+
+insertUser("Prayash","dfsdf")
+insertUser("Prayashdfsfa","ddsaffsdf")
+insertUser("fdsfaPrayash","deiruioefsdf")
+y=IsUser("Prayash","dfsdfs")
+print(y)
+x=input()
+drop()
